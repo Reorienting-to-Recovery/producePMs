9b9d21e3bdaeab0ca44cadfbd24507222d8d63ad
Erin Cain <ecain@flowwest.com>
2023-06-09 09:55:09 -0700

    Adds more pms to model results df

--- R/process_model_results.R
+++ R/process_model_results.R
@@ -10,7 +10,7 @@ library(tidyverse)
 #'                                                      seeds = baseline_seeds)
 #' process_model_results(baseline_model_results, "Baseline")
 #' @export
-create_model_results_dataframe <- function(model_results, scenario_name, chinook_run) {
+create_model_results_dataframe <- function(model_results, model_parameters, scenario_name, chinook_run) {
   nat_spawn <- dplyr::as_tibble(model_results$spawners * model_results$proportion_natural_at_spawning) |>
     dplyr::mutate(location = fallRunDSM::watershed_labels) |>
     pivot_longer(cols = c(`1`:`20`), values_to = "value", names_to = "year") |>
@@ -47,10 +47,61 @@ create_model_results_dataframe <- function(model_results, scenario_name, chinook
     summarise(value = sum(juveniles)) |>
     mutate(preformance_metric = "Juveniles") |> glimpse()
 
+  names_spawning_watersheds <- switch(chinook_run,
+                                       "fall" = names(which(ifelse(fallRunDSM::adult_seeds[, 1] == 0, FALSE, TRUE))),
+                                       "spring" = names(which(ifelse(springRunDSM::adult_seeds[, 1] == 0, FALSE, TRUE))),
+                                       "winter" = names(which(ifelse(winterRunDSM::adult_seeds[, 1] == 0, FALSE, TRUE))))
+
+  # add marine derived nutrient stuff in
+  chanel_area <- read_csv("data-raw/channel_areas.csv") |>
+    mutate(sit_width = `width - sit` * 0.3048,
+           sit_length = `length - sit` * 0.3048,
+           add_max_hab_width = `width - add tmh`* 0.3048,
+           add_max_hab_length = `length - add tmh` * 0.3048,
+           sit_area = sit_width * sit_length,
+           add_tmh_area = add_max_hab_width * add_max_hab_length)
+
+  sit_total_area_sqmt <- chanel_area |>
+    pull(sit_area) |>
+    sum(na.rm = T)
+
+  max_hab_total_area_sqmt <- chanel_area |>
+    pull(add_tmh_area) |>
+    sum(na.rm = T) + sit_total_area_sqmt
+  # prep hab capacity stuff
+  spawning_months <- switch(selected_run,
+                            "fall" = c(10:12),
+                            "spring" = c(7:10),
+                            "winter" = c(5:7),
+                            "late fall" = c(10:12, 1:2))
+  month_lookup <- tibble(month_num = c(1:12), month = month.abb)
+  year_lookup <- tibble(year = as.character(1979:2000), model_year = 1:22)
+  spawn_capacity <- model_parameters$spawning_habitat |>
+    as_tibble() |>
+    mutate(location = fallRunDSM::watershed_labels) |>
+    pivot_longer(cols = -location, names_to = "year_date", values_to = "spawning_habitat") |>
+    separate(year_date, into = c("month", "year")) |>
+    left_join(month_lookup) |>
+    left_join(year_lookup) |>
+    mutate(year = model_year) |>
+    select(-model_year) |>
+    filter(month_num  %in% spawning_months) |>
+    group_by(year, month, location) |> # can add location in here too if we want
+    summarise(total_monthly_spawning_habitat = sum(spawning_habitat, na.rm = TRUE)) |>
+    ungroup() |>
+    group_by(year, location) |>
+    summarise(average_monthly_annual_spawning_habitat = mean(total_monthly_spawning_habitat, na.rm = TRUE)) |>
+    ungroup() |>
+    mutate(spawner_capacity = round(average_monthly_annual_spawning_habitat/
+                                      fallRunDSM::r_to_r_baseline_params$spawn_success_redd_size),
+           year = as.numeric(year)) |>
+    select(year, location, spawner_capacity)
+
   result_dataframe <- bind_rows(nat_spawn, all_spawn, juveniles, phos) |>
     mutate(scenario = scenario_name,
            run = chinook_run) |>
     pivot_wider(values_from = value, names_from = preformance_metric) |>
+    left_join(spawn_capacity) |>
     arrange(location, year) |>
     group_by(location) |>
     mutate(nat_spawners_lead = lead(`Natural Spawners`, 3),
@@ -58,11 +109,16 @@ create_model_results_dataframe <- function(model_results, scenario_name, chinook
            "CRR: Juvenile to Natural Adult" =  nat_spawners_lead / `Juveniles`,
            "CRR: Total Adult to Returning Natural Adult" = nat_spawners_lead / `All Spawners`,
            "Growth Rate Natural Spawners" = (`Natural Spawners` - lag(`Natural Spawners`, 1) ) / lag(`Natural Spawners`, 1),
-           "Independent Population" = ifelse(`Natural Spawners` > 500 & `PHOS` < .05 & `Growth Rate Natural Spawners` > 1 & `CRR: Total Adult to Returning Natural Adult` > 1, TRUE, FALSE)
+           "Independent Population" = ifelse(`Natural Spawners` > 500 & `PHOS` < .05 & `Growth Rate Natural Spawners` > 1 & `CRR: Total Adult to Returning Natural Adult` > 1, TRUE, FALSE),
+           "Dependent Populations" = ifelse(location %in% names_spawning_watersheds & !`Independent Population`, TRUE, FALSE),
+           area_sqmt = ifelse(scenario == "Max Habitat",
+                               max_hab_total_area_sqmt, sit_total_area_sqmt),
+           "Marine Derived Nutrient (pounds per sq meter)" = (`All Spawners` * 21) / area_sqmt,
+           "Carrying Capacity vs Abundance" = `All Spawners` / spawner_capacity
     ) |>
     ungroup() |>
-    select(-nat_spawners_lead) |>
-    pivot_longer(cols = 5:12, names_to = "performance_metric", values_to = "value") |> glimpse()
+    select(-nat_spawners_lead, -area_sqmt, -spawner_capacity) |>
+    pivot_longer(cols = 5:15, names_to = "performance_metric", values_to = "value") |> glimpse()
 
   # add juv and adult df
   adults_age <- model_results$returning_adults |>
