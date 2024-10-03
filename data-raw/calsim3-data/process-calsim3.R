14b1bad9e563e33f2d9661574de1f65a343d2bbb
ergz <erodriguez@flowwest.com>
2024-10-03 12:42:26 -0700

    adds storage locations from calsim3 nodes
    this still needs to grab storage for Friant Damn

--- data-raw/calsim3-data/process-calsim3.R
+++ data-raw/calsim3-data/process-calsim3.R
@@ -1,7 +1,29 @@
 library(tidyverse)
 library(readxl)
+library(here)
 
-raw_storage <- read_excel("data-raw/calsim3-data/HRL-storage.xlsx")
+# Storage data -----------------------------------------------------------------
+raw_storage_path <- here("data-raw", "calsim3-data", "HRL-storage.xlsx")
+raw_storage <- read_excel(raw_storage_path)
+
+# all nodes
+colnames(raw_storage)[str_detect(colnames(raw_storage), "FR")]
 
 raw_storage |>
-  select(date, S_WKYTN, S_KSWCK)
+  select(date, S_WKYTN, S_KSWCK, S_THRMA, S_OROVL, S_ENGLB,S_FOLSM, S_MELON, S_PEDRO,
+         S_MCLRE, S_NHGAN, S_SGRGE, S_SHSTA)
+
+
+storage_locations_from_calsim2 <- c("Wiskeytown Lake",
+                                    "Shasta Lake",
+                                    "Keswich Reservoir",
+                                    'Thermalito Complex',
+                                    "Lake Oroville",
+                                    "Stony Gorge",
+                                    "Engilbright",
+                                    "Folsom",
+                                    "New Hogan",
+                                    "New Melones",
+                                    "New Don Padro",
+                                    "Lake McClure",
+                                    "Friant")
