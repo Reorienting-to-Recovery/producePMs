5c8c474a0ef5bb7ebe6416d9498a2236d0f1d330
ergz <erodriguez@flowwest.com>
2024-10-03 12:49:01 -0700

    useful comments

--- data-raw/calsim3-data/process-calsim3.R
+++ data-raw/calsim3-data/process-calsim3.R
@@ -6,24 +6,12 @@ library(here)
 raw_storage_path <- here("data-raw", "calsim3-data", "HRL-storage.xlsx")
 raw_storage <- read_excel(raw_storage_path)
 
-# all nodes
+# replace the string with a regex to id potential nodes
 colnames(raw_storage)[str_detect(colnames(raw_storage), "FR")]
 
+# thes are potential mappings for each of the storage locations in the original script
+# TODO - missing Friant Dam!!!
 raw_storage |>
   select(date, S_WKYTN, S_KSWCK, S_THRMA, S_OROVL, S_ENGLB,S_FOLSM, S_MELON, S_PEDRO,
          S_MCLRE, S_NHGAN, S_SGRGE, S_SHSTA)
 
-
-storage_locations_from_calsim2 <- c("Wiskeytown Lake",
-                                    "Shasta Lake",
-                                    "Keswich Reservoir",
-                                    'Thermalito Complex',
-                                    "Lake Oroville",
-                                    "Stony Gorge",
-                                    "Engilbright",
-                                    "Folsom",
-                                    "New Hogan",
-                                    "New Melones",
-                                    "New Don Padro",
-                                    "Lake McClure",
-                                    "Friant")
